cd728c2eca943a241a56dfc058fc25b3413b1614
Will Hoback <will.hoback@gmail.com>
2019-09-15 15:47:09 -0400

    list and struct

--- pset1/m61.cc
+++ pset1/m61.cc
@@ -6,6 +6,12 @@
 #include <cinttypes>
 #include <cassert>
 
+//track stats
+m61_statistics global_stats = {0,0,0,0,0,0,0,0};
+
+//STL list to hold ptrs to metadata structs
+std::list<meta *> metadata_list;
+
 /// m61_malloc(sz, file, line)
 ///    Return a pointer to `sz` bytes of newly-allocated dynamic memory.
 ///    The memory is not initialized. If `sz == 0`, then m61_malloc must
@@ -16,7 +22,30 @@ void *m61_malloc(size_t sz, const char *file, long line)
 {
     (void)file, (void)line; // avoid uninitialized variable warnings
     // Your code here.
-    return base_malloc(sz);
+    void* ptr_to_base_malloc;
+    //initialize metadata struct
+    meta allocation_metadata;
+    allocation_metadata.size = sz;
+    unsigned int size_of_metadata_struct = sizeof(meta);
+    
+    
+
+    // test005 make sure sz requested isn't too large to handle
+    if (!sz || sz > 0x10000000000)
+    {
+        global_stats.nfail++;
+        global_stats.fail_size += sz;
+        return nullptr;
+    }
+    ptr_to_base_malloc = base_malloc(sz);
+    //test003 track actives but we also need to free them
+    global_stats.nactive++;
+    //test002 track allocation totals
+    global_stats.ntotal++;
+    //test004 total allocation size
+    global_stats.total_size += sz;
+
+    return ptr_to_base_malloc;
 }
 
 /// m61_free(ptr, file, line)
@@ -29,6 +58,10 @@ void m61_free(void *ptr, const char *file, long line)
     (void)file, (void)line; // avoid uninitialized variable warnings
     // Your code here.
     base_free(ptr);
+    //test003 freeing an alloc means we need to remove it from our stats
+    global_stats.nactive--;
+    //test006 make sure to update active allocation size when freeing
+    global_stats.active_size -= 
 }
 
 /// m61_calloc(nmemb, sz, file, line)
@@ -57,6 +90,8 @@ void m61_get_statistics(m61_statistics *stats)
     // Stub: set all statistics to enormous numbers
     memset(stats, 255, sizeof(m61_statistics));
     // Your code here.
+    //test001 - init stats tracking
+    *stats = global_stats;
 }
 
 /// m61_print_statistics()
