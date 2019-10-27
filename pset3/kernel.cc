eb83f69a9aa858c812d869de2082a69f145e925f
whoback <will.hoback@gmail.com>
2019-10-27 14:32:10 -0400

    some semblance of iso

--- pset3/kernel.cc
+++ pset3/kernel.cc
@@ -7,7 +7,6 @@
 //
 //    This is the kernel.
 
-
 // INITIAL PHYSICAL MEMORY LAYOUT
 //
 //  +-------------- Base Memory --------------+
@@ -21,15 +20,14 @@
 //                                             | \___ PROC_SIZE ___/
 //                                      PROC_START_ADDR
 
-#define PROC_SIZE 0x40000       // initial state only
+#define PROC_SIZE 0x40000 // initial state only
 
-proc ptable[NPROC];             // array of process descriptors
-                                // Note that `ptable[0]` is never used.
-proc* current;                  // pointer to currently executing proc
+proc ptable[NPROC]; // array of process descriptors
+                    // Note that `ptable[0]` is never used.
+proc *current;      // pointer to currently executing proc
 
-#define HZ 100                  // timer interrupt frequency (interrupts/sec)
+#define HZ 100                           // timer interrupt frequency (interrupts/sec)
 static std::atomic<unsigned long> ticks; // # timer interrupts so far
-
 
 // Memory state
 //    Information about physical page with address `pa` is stored in
@@ -39,21 +37,20 @@ static std::atomic<unsigned long> ticks; // # timer interrupts so far
 
 pageinfo pages[NPAGES];
 
-
 [[noreturn]] void schedule();
-[[noreturn]] void run(proc* p);
-void exception(regstate* regs);
-uintptr_t syscall(regstate* regs);
+[[noreturn]] void run(proc *p);
+void exception(regstate *regs);
+uintptr_t syscall(regstate *regs);
 void memshow();
-
 
 // kernel(command)
 //    Initialize the hardware and processes and start running. The `command`
 //    string is an optional string passed from the boot loader.
 
-static void process_setup(pid_t pid, const char* program_name);
+static void process_setup(pid_t pid, const char *program_name);
 
-void kernel(const char* command) {
+void kernel(const char *command)
+{
     // initialize hardware
     init_hardware();
     log_printf("Starting WeensyOS\n");
@@ -65,25 +62,35 @@ void kernel(const char* command) {
     console_clear();
 
     // (re-)initialize kernel page table
-    for (vmiter it(kernel_pagetable); it.va() < MEMSIZE_VIRTUAL; it += PAGESIZE) {
-        if (it.va() >= PROC_START_ADDR || it.va() == CONSOLE_ADDR) {
+    for (vmiter it(kernel_pagetable); it.va() < MEMSIZE_VIRTUAL; it += PAGESIZE)
+    {
+        if (it.va() >= PROC_START_ADDR || it.va() == CONSOLE_ADDR)
+        {
             it.map(it.va(), PTE_P | PTE_W | PTE_U);
-        } else if (it.va() != 0) {
-             it.map(it.va(), PTE_P | PTE_W);
-        } else {
+        }
+        else if (it.va() != 0)
+        {
+            it.map(it.va(), PTE_P | PTE_W);
+        }
+        else
+        {
             // nullptr is inaccessible even to the kernel
             it.map(it.va(), 0);
         }
     }
 
     // set up process descriptors
-    for (pid_t i = 0; i < NPROC; i++) {
+    for (pid_t i = 0; i < NPROC; i++)
+    {
         ptable[i].pid = i;
         ptable[i].state = P_FREE;
     }
-    if (command && program_loader(command).present()) {
+    if (command && program_loader(command).present())
+    {
         process_setup(1, command);
-    } else {
+    }
+    else
+    {
         process_setup(1, "allocator");
         process_setup(2, "allocator2");
         process_setup(3, "allocator3");
@@ -93,7 +100,6 @@ void kernel(const char* command) {
     // Switch to the first process using run()
     run(&ptable[1]);
 }
-
 
 // kalloc(sz)
 //    Kernel memory allocator. Allocates `sz` contiguous bytes and
@@ -112,131 +118,125 @@ void kernel(const char* command) {
 
 static uintptr_t next_alloc_pa;
 
-void* kalloc(size_t sz) {
-    if (sz > PAGESIZE) {
+void *kalloc(size_t sz)
+{
+    if (sz > PAGESIZE)
+    {
         return nullptr;
     }
 
-    while (next_alloc_pa < MEMSIZE_PHYSICAL) {
+    while (next_alloc_pa < MEMSIZE_PHYSICAL)
+    {
         uintptr_t pa = next_alloc_pa;
         next_alloc_pa += PAGESIZE;
 
-        if (allocatable_physical_address(pa)
-            && !pages[pa / PAGESIZE].used()) {
+        if (allocatable_physical_address(pa) && !pages[pa / PAGESIZE].used())
+        {
             pages[pa / PAGESIZE].refcount = 1;
-            memset((void*) pa, 0xCC, PAGESIZE);
-            return (void*) pa;
+            memset((void *)pa, 0xCC, PAGESIZE);
+            return (void *)pa;
         }
     }
     return nullptr;
 }
 
-
 // kfree(kptr)
 //    Free `kptr`, which must have been previously returned by `kalloc`.
 //    If `kptr == nullptr` does nothing.
 
-void kfree(void* kptr) {
+void kfree(void *kptr)
+{
     // assert(pages[(uintptr_t)kptr / PAGESIZE].refcount == 1);
-    if(kptr == nullptr)
+    if (kptr == nullptr)
     {
         return;
     }
-    if(allocatable_physical_address((uintptr_t) kptr)
-        && pages[(uintptr_t)kptr / PAGESIZE].refcount != 0)
+    if (allocatable_physical_address((uintptr_t)kptr) && pages[(uintptr_t)kptr / PAGESIZE].refcount != 0)
     {
         pages[(uintptr_t)kptr / PAGESIZE].refcount = 0;
-    } 
+    }
     else
     {
         return;
     }
-    
-    
 }
-
 
 // process_setup(pid, program_name)
 //    Load application program `program_name` as process number `pid`.
 //    This loads the application's code and data into memory, sets its
 //    %rip and %rsp, gives it a stack page, and marks it as runnable.
 
-void process_setup(pid_t pid, const char* program_name) {
+void process_setup(pid_t pid, const char *program_name)
+{
     init_process(&ptable[pid], 0);
     set_pagetable(kernel_pagetable);
-    
+
     uintptr_t first_addr = PROC_START_ADDR + (pid - 1) * PROC_SIZE;
     uintptr_t last_addr = PROC_START_ADDR + pid * PROC_SIZE;
     unsigned pageno = 1 + (pid - 1) * 4;
-    x86_64_pagetable* pt = (x86_64_pagetable*) kalloc(PAGESIZE);
-     memset(pt, 0, PAGESIZE);
-  ptable[pid].pagetable = pt;
-    for (vmiter it(pt, 0); it.va() != PROC_START_ADDR; it += PAGESIZE) {
-        it.map(it.va(), PTE_P | PTE_W);
+    x86_64_pagetable *pt = (x86_64_pagetable *)kalloc(PAGESIZE);
+    memset(pt, 0, PAGESIZE);
+    ptable[pid].pagetable = pt;
+    for (vmiter ita(kernel_pagetable, 0), itb(ptable[pid].pagetable, 0);
+        ita.va() < MEMSIZE_VIRTUAL;
+        ita += PAGESIZE, itb += PAGESIZE) {
+            if (itb.va() < PROC_START_ADDR)
+                {
+                    itb.map(ita.pa(), ita.perm());
+            } else if (itb.va() != 0) {
+                itb.map(ita.pa(), PTE_P | PTE_W);
+            } else {
+                // nullptr is inaccessible even to the kernel
+                itb.map(ita.pa(), 0);
+            }
     }
-    vmiter(pt, CONSOLE_ADDR).map(CONSOLE_ADDR, PTE_P | PTE_W | PTE_U);
-    for (uintptr_t a = first_addr; a != last_addr; a += PAGESIZE) {
-        vmiter(pt, a).map(a, PTE_P | PTE_W | PTE_U);
-    }
-    
-   
+    // for (vmiter it(pt, 0); it.va() != PROC_START_ADDR; it += PAGESIZE) {
+    //     it.map(it.va(), PTE_P | PTE_W);
+    // }
+    // vmiter(pt, CONSOLE_ADDR).map(CONSOLE_ADDR, PTE_P | PTE_W | PTE_U);
+    // for (uintptr_t a = first_addr; a != last_addr; a += PAGESIZE) {
+    //     vmiter(pt, a).map(a, PTE_P | PTE_W | PTE_U);
+    // }
 
- 
-    set_pagetable(ptable[pid].pagetable);
     // load the program
     program_loader loader(program_name);
 
     // allocate and map all memory
-     for (loader.reset(); loader.present(); ++loader) {
+       for (loader.reset(); loader.present(); ++loader) {
         for (uintptr_t a = round_down(loader.va(), PAGESIZE);
              a < loader.va() + loader.size();
              a += PAGESIZE) {
+            assert(a >= first_addr && a < last_addr);
             assert(!pages[a / PAGESIZE].used());
-            void* dstpg = kalloc(PAGESIZE);
-            memset(dstpg, 0, PAGESIZE);
-            memcpy((void *) dstpg, loader.data(), loader.data_size());
-                 int t =  vmiter(pt, a).try_map((uintptr_t)dstpg, PTE_P | PTE_W | PTE_U);
-                if(t!=0)
-                 {
-                  console_printf("error trymap");
-                    }
-              }
-        console_printf("Made it to lader\n");
-
+            void *p = kalloc(PAGESIZE);
+            memset(p, 0, PAGESIZE);
+            memcpy(p, loader.data(), loader.data_size());
+            vmiter(ptable[pid].pagetable, a).map((uintptr_t)p, PTE_P | PTE_W | PTE_U); 
+        }
     }
-    
-console_printf("Out of loader.\n");
+
+    // console_printf("Out of loader.\n");
     // copy instructions and data into place
-    for (loader.reset(); loader.present(); ++loader) {
-        memset((void*) loader.va(), 0, loader.size());
-        memcpy((void*) loader.va(), loader.data(), loader.data_size());
+    for (loader.reset(); loader.present(); ++loader)
+    {
+        memset((void *)loader.va(), 0, loader.size());
+        memcpy((void *)loader.va(), loader.data(), loader.data_size());
     }
-
     // mark entry point
     ptable[pid].regs.reg_rip = loader.entry();
 
-    // allocate stack
-    uintptr_t stack_addr = PROC_START_ADDR + PROC_SIZE * pid - PAGESIZE;
+      // allocate stack
+    uintptr_t stack_addr = last_addr - PAGESIZE;
     assert(!pages[stack_addr / PAGESIZE].used());
-     pages[stack_addr / PAGESIZE].refcount = 1;
+    // pages[stack_addr / PAGESIZE].refcount = 1;
+     void *p = kalloc(PAGESIZE);
+            memset(p, 0, PAGESIZE);
+            vmiter(ptable[pid].pagetable, stack_addr).map((uintptr_t)p, PTE_P | PTE_W | PTE_U);
     ptable[pid].regs.reg_rsp = stack_addr + PAGESIZE;
-    // //  vmiter t(pt);
-    // //  t.map(stack_addr, PTE_P | PTE_W | PTE_U);
-    void* dstpg = kalloc(PAGESIZE);
-    memset(dstpg, 0, PAGESIZE);
-    //memcpy(dstpg, (void*)stack_addr, PAGESIZE);
-
-    int t = vmiter(pt).try_map(stack_addr, PTE_P | PTE_W | PTE_U);
-        if(t != 0)
-        {
-            console_printf("stack addr fail");
-        }
     // mark process as runnable
     ptable[pid].state = P_RUNNABLE;
-     console_printf("Stack");
+    // console_printf("Stack");
 }
-
-
 
 // exception(regs)
 //    Exception handler (for interrupts, traps, and faults).
@@ -249,7 +249,8 @@ console_printf("Out of loader.\n");
 //
 //    Note that hardware interrupts are disabled when the kernel is running.
 
-void exception(regstate* regs) {
+void exception(regstate *regs)
+{
     // Copy the saved registers into the `current` process descriptor.
     current->regs = *regs;
     regs = &current->regs;
@@ -262,32 +263,37 @@ void exception(regstate* regs) {
     // Show the current cursor location and memory state
     // (unless this is a kernel fault).
     console_show_cursor(cursorpos);
-    if (regs->reg_intno != INT_PF || (regs->reg_errcode & PFERR_USER)) {
+    if (regs->reg_intno != INT_PF || (regs->reg_errcode & PFERR_USER))
+    {
         memshow();
     }
 
     // If Control-C was typed, exit the virtual machine.
     check_keyboard();
 
-
     // Actually handle the exception.
-    switch (regs->reg_intno) {
+    switch (regs->reg_intno)
+    {
 
     case INT_IRQ + IRQ_TIMER:
         ++ticks;
         lapicstate::get().ack();
         schedule();
-        break;                  /* will not be reached */
+        break; /* will not be reached */
 
-    case INT_PF: {
+    case INT_PF:
+    {
         // Analyze faulting address and access type.
         uintptr_t addr = rdcr2();
-        const char* operation = regs->reg_errcode & PFERR_WRITE
-                ? "write" : "read";
-        const char* problem = regs->reg_errcode & PFERR_PRESENT
-                ? "protection problem" : "missing page";
+        const char *operation = regs->reg_errcode & PFERR_WRITE
+                                    ? "write"
+                                    : "read";
+        const char *problem = regs->reg_errcode & PFERR_PRESENT
+                                  ? "protection problem"
+                                  : "missing page";
 
-        if (!(regs->reg_errcode & PFERR_USER)) {
+        if (!(regs->reg_errcode & PFERR_USER))
+        {
             panic("Kernel page fault for %p (%s %s, rip=%p)!\n",
                   addr, operation, problem, regs->reg_rip);
         }
@@ -300,18 +306,18 @@ void exception(regstate* regs) {
 
     default:
         panic("Unexpected exception %d!\n", regs->reg_intno);
-
     }
 
-
     // Return to the current process (or run something else).
-    if (current->state == P_RUNNABLE) {
+    if (current->state == P_RUNNABLE)
+    {
         run(current);
-    } else {
+    }
+    else
+    {
         schedule();
     }
 }
-
 
 // syscall(regs)
 //    System call handler.
@@ -323,7 +329,8 @@ void exception(regstate* regs) {
 
 int syscall_page_alloc(uintptr_t addr);
 
-uintptr_t syscall(regstate* regs) {
+uintptr_t syscall(regstate *regs)
+{
     // Copy the saved registers into the `current` process descriptor.
     current->regs = *regs;
     regs = &current->regs;
@@ -341,34 +348,33 @@ uintptr_t syscall(regstate* regs) {
     // If Control-C was typed, exit the virtual machine.
     check_keyboard();
 
-
     // Actually handle the exception.
-    switch (regs->reg_rax) {
+    switch (regs->reg_rax)
+    {
 
     case SYSCALL_PANIC:
-        panic(nullptr);         // does not return
+        panic(nullptr); // does not return
 
     case SYSCALL_GETPID:
         return current->pid;
 
     case SYSCALL_YIELD:
         current->regs.reg_rax = 0;
-        schedule();             // does not return
+        schedule(); // does not return
 
     case SYSCALL_PAGE_ALLOC:
     {
         //uintptr_t addr = current->regs.reg_rdi;
-      
 
         // void* pg = kalloc(PAGESIZE);
-        // if (!pg) 
-        // { 
+        // if (!pg)
+        // {
         //     // no free physical pages
         // console_printf(CPOS(24, 0), 0x0C00, "Out of physical memory!\n");
         // return -1;
         // }
 
-        // if (vmiter(current, addr).user()) 
+        // if (vmiter(current, addr).user())
         // {
         //     kfree((void*) vmiter(current, addr).pa());
         // }
@@ -379,62 +385,63 @@ uintptr_t syscall(regstate* regs) {
     }
     default:
         panic("Unexpected system call %ld!\n", regs->reg_rax);
-
     }
 
     panic("Should not get here!\n");
 }
-
 
 // syscall_page_alloc(addr)
 //    Handles the SYSCALL_PAGE_ALLOC system call. This function
 //    should implement the specification for `sys_page_alloc`
 //    in `u-lib.hh` (but in the handout code, it does not).
 
-int syscall_page_alloc(uintptr_t addr) {
+int syscall_page_alloc(uintptr_t addr)
+{
     //console_printf("It got to syscall_page_alloc");
-      if ((addr % PAGESIZE != 0 || addr < PROC_START_ADDR || addr >= MEMSIZE_VIRTUAL) && addr != CONSOLE_ADDR) 
-        {
-            return -1;
-        }
+    if ((addr % PAGESIZE != 0) || (addr < PROC_START_ADDR) || (addr >= MEMSIZE_VIRTUAL))
+    {
+        return -1;
+    }
 
-        if (vmiter(current, addr).user()) 
-        {
-            kfree((void*) vmiter(current, addr).pa());
-        }
-       assert(!pages[addr / PAGESIZE].used());
+    if (vmiter(current, addr).user())
+    {
+        kfree((void *)vmiter(current, addr).pa());
+    }
+    assert(!pages[addr / PAGESIZE].used());
 
-       void* p =  kalloc(PAGESIZE);
-       if(!p)
-       {
-           console_printf("No phys memory for sycall");
-           return -1;
-       }
-       memset((void*) p, 0, PAGESIZE);
-       memcpy((void*)addr, p, PAGESIZE);
-    
+    void *p = kalloc(PAGESIZE);
+    if (!p)
+    {
+        // console_printf("No phys memory for sycall");
+        return -1;
+    }
+    memset((void *)p, 0, PAGESIZE);
+    memcpy((void *)addr, p, PAGESIZE);
+
     // console_printf("in sycall\n");
     pid_t pid = current->pid;
-   int t =  vmiter(ptable[pid].pagetable, addr).try_map((uintptr_t)p, PTE_P | PTE_W | PTE_U);
-   if(t!=0)
-   {
-                  console_printf("error trymap");
+    int t = vmiter(ptable[pid].pagetable, addr).try_map((uintptr_t)p, PTE_P | PTE_W | PTE_U);
+    if (t != 0)
+    {
+        // console_printf("error trymap");
 
-       return -1;
-   }
+        return -1;
+    }
     return 0;
 }
-
 
 // schedule
 //    Pick the next process to run and then run it.
 //    If there are no runnable processes, spins forever.
 
-void schedule() {
+void schedule()
+{
     pid_t pid = current->pid;
-    for (unsigned spins = 1; true; ++spins) {
+    for (unsigned spins = 1; true; ++spins)
+    {
         pid = (pid + 1) % NPROC;
-        if (ptable[pid].state == P_RUNNABLE) {
+        if (ptable[pid].state == P_RUNNABLE)
+        {
             run(&ptable[pid]);
         }
 
@@ -442,19 +449,20 @@ void schedule() {
         check_keyboard();
 
         // If spinning forever, show the memviewer.
-        if (spins % (1 << 12) == 0) {
+        if (spins % (1 << 12) == 0)
+        {
             memshow();
             log_printf("%u\n", spins);
         }
     }
 }
 
-
 // run(p)
 //    Run process `p`. This involves setting `current = p` and calling
 //    `exception_return` to restore its page table and registers.
 
-void run(proc* p) {
+void run(proc *p)
+{
     assert(p->state == P_RUNNABLE);
     current = p;
 
@@ -466,36 +474,41 @@ void run(proc* p) {
     exception_return(p);
 
     // should never get here
-    while (true) {
+    while (true)
+    {
     }
 }
-
 
 // memshow()
 //    Draw a picture of memory (physical and virtual) on the CGA console.
 //    Switches to a new process's virtual memory map every 0.25 sec.
 //    Uses `console_memviewer()`, a function defined in `k-memviewer.cc`.
 
-void memshow() {
+void memshow()
+{
     static unsigned last_ticks = 0;
     static int showing = 0;
 
     // switch to a new process every 0.25 sec
-    if (last_ticks == 0 || ticks - last_ticks >= HZ / 2) {
+    if (last_ticks == 0 || ticks - last_ticks >= HZ / 2)
+    {
         last_ticks = ticks;
         showing = (showing + 1) % NPROC;
     }
 
-    proc* p = nullptr;
-    for (int search = 0; !p && search < NPROC; ++search) {
-        if (ptable[showing].state != P_FREE
-            && ptable[showing].pagetable) {
+    proc *p = nullptr;
+    for (int search = 0; !p && search < NPROC; ++search)
+    {
+        if (ptable[showing].state != P_FREE && ptable[showing].pagetable)
+        {
             p = &ptable[showing];
-        } else {
+        }
+        else
+        {
             showing = (showing + 1) % NPROC;
         }
     }
 
-    extern void console_memviewer(proc* vmp);
+    extern void console_memviewer(proc * vmp);
     console_memviewer(p);
 }
